fabad0bf37710686ebccd01e87ea465ea24d6ea9
Barsharani Patra <barsharani88071@gmail.com>
2026-05-13 00:20:12 +0530

    Delete GrievanceHandlingSystem/src/com/ghs directory

--- GrievanceHandlingSystem/src/com/ghs/main/Main.java
+++
@@ -1,8 +0,0 @@
-package com.ghs.main;
-
-public class Main {
-    public static void main(String[] args) {
-        GrievanceController controller = new GrievanceController();
-        controller.startSystem();
-    }
-}
